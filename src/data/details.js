911918f712ece7d7d555224f35bf68a6f3e02db4
Phil Hawksworth <phil@netify.com>
2018-10-21 17:53:09 +0100

    devine the github URL

--- src/data/details.js
+++ src/data/details.js
@@ -17,11 +17,15 @@ module.exports = {
   event: "Smashing Conference",
 
   // The repository to which this site is linked
-  repo : process.env.REPOSITORY_URL || 'https://github.com/netlify/my-first-few-deploys',
+  repo : process.env.REPOSITORY_URL
+    ?  process.env.REPOSITORY_URL.replace(':','/').replace('git@', 'https://')
+    : 'https://github.com/netlify/my-first-few-deploys',
 
   // The site name used in the URL of the Netlify Admin UI
   // (Which we can figure out from the deployed URL)
-  sitename: process.env.URL ? process.env.URL.split('https://')[1].split('.netlify.com')[0] : 'playwith',
+  sitename: process.env.URL
+    ? process.env.URL.split('https://')[1].split('.netlify.com')[0]
+    : 'playwith',
 
   // Something to tweet which includes a link to
   // this instance of the example site
